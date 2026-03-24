641f15fc3899e05768367174a37aa34f01bf0bee
Fu22210 <kariagin.tm@phystech.edu>
2026-03-24 13:33:47 +0300

    first_try_baseline

---
+++ my_network_rnn.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import random
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+        
+        self.embedding = nn.Embedding(input_dim, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, src):
+        # src = [src sent len, batch size]
+        embedded = self.dropout(self.embedding(src))
+        # embedded = [src sent len, batch size, emb dim]
+        
+        outputs, (hidden, cell) = self.rnn(embedded)
+        return hidden, cell
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout):
+        super().__init__()
+        self.output_dim = output_dim
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+        
+        self.embedding = nn.Embedding(output_dim, emb_dim)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
+        self.out = nn.Linear(hid_dim, output_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, input, hidden, cell):
+        input = input.unsqueeze(0)
+        
+        embedded = self.dropout(self.embedding(input))
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
+
+        prediction = self.out(output.squeeze(0))
+        return prediction, hidden, cell
+
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+        
+    def forward(self, src, trg, teacher_forcing_ratio = 0.5):
+        # trg = [trg sent len, batch size]
+        batch_size = trg.shape[1]
+        max_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+        
+        outputs = torch.zeros(max_len, batch_size, trg_vocab_size).to(self.device)
+        
+        hidden, cell = self.encoder(src)
+        
+        input = trg[0,:]
+        
+        for t in range(1, max_len):
+            output, hidden, cell = self.decoder(input, hidden, cell)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.argmax(1)
+            input = trg[t] if teacher_force else top1
+            
+        return outputs
